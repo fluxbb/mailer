6a9149069a02ca1821dfcd9dee919c191798694b
Jamie Furness <jfurness@yammer-inc.com>
2014-02-16 12:41:54 +0000

    Fixing utf8 dependency

--- tests/MailerTest.php
+++ tests/MailerTest.php
@@ -27,6 +27,8 @@
 
 namespace fluxbb\mailer\tests;
 
+require_once 'Patchwork/Utf8.php';
+
 require_once dirname(__FILE__).'/../src/FluxBB/Mailer/Mailer.php';
 require_once dirname(__FILE__).'/../src/FluxBB/Mailer/Email.php';
 require_once dirname(__FILE__).'/../src/FluxBB/Mailer/Exception.php';
